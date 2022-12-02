ac035824ea727a772af56cf95d53e44481bc3b0c
alvarofeher <alvarofeherargullos@gmail.com>
2022-12-02 13:39:44 +0100

    approach 2

---
+++ day02/approach2.py
@@ -0,0 +1,12 @@
+
+totalPoints = 0
+plays = []  # array storing both plays in a round -> plays[A,X]
+
+with open("day02/input.txt") as file:
+    rounds = [i for i in file.read().strip().split("\n")]
+
+for round in rounds:
+    plays = round.split()
+    if plays[0] == "A" & plays[1] == "X":
+        totalPoints = totalPoints + 4
+    # do al cases and that's it, way better with the dictionary

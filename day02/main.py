0e230ada4ce2fd2efbc599ba539e725b6182d298
alvarofeher <alvarofeherargullos@gmail.com>
2022-12-02 13:19:59 +0100

    part 1

---
+++ day02/main.py
@@ -0,0 +1,14 @@
+
+
+totalPoints = 0
+with open("day02/input.txt") as file:
+    rounds = [i for i in file.read().strip().split("\n")]
+
+# python dictionary with possible outcomes
+possibleSolutions = {
+    "A X": 4, "A Y": 8, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6
+}
+for round in rounds:
+    totalPoints = totalPoints + possibleSolutions[round]
+
+print(totalPoints)

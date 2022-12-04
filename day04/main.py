8f7868320025c60c90b1e434391a0d5b11dfb31f
alvarofeher <alvarofeherargullos@gmail.com>
2022-12-04 09:38:50 +0100

    part1

---
+++ day04/main.py
@@ -0,0 +1,17 @@
+with open("day04/input.txt") as file:
+    pairs = [i for i in file.read().strip().split("\n")]
+count = 0
+for pair in pairs:
+    margins = pair.split(",")  # get both margins
+    p1 = margins[0].split("-")  # get limits of p1
+    p2 = margins[1].split("-")  # get limits of p2
+    # convert to int
+    a = int(p1[0])
+    b = int(p1[1])
+    c = int(p2[0])
+    d = int(p2[1])
+    # check
+    if ((a <= c) and (b >= d)) or ((a >= c) and (b <= d)):
+        count += 1
+
+print(count)
